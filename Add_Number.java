af97ba910a15ebbd3e2d0f614ac70681bad93560
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-23 11:22:29 +0530

    Add files via upload

---
+++ Add_Number.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Add_Number {
+public static void main(String[] args) throws Exception
+{
+	BufferedReader obj = 
+	new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Enter String :");
+	String name = obj.readLine(); // suresh
+	System.out.println("My name is = "+ name);
+} 
+}
